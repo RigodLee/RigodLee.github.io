108a6b1b8ba94151491e8d0484cd98a4257f36b5
RigodLee <852195914@qq.com>
2020-10-02 14:25:37 +0800

    Site updated: 2020-10-02 14:25:06

--- js/shadow.js
+++
@@ -1,4 +0,0 @@
-// 设置页脚博主会动的心
-$(document).ready(function(e){
-    $('.copyright').html('©2019 - 2020 <img src="https://c-ssl.duitang.com/uploads/item/201601/08/20160108151722_JC3dV.thumb.1000_0.gif" />shadow');
-})
