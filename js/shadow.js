ea7b274c1e7f221a1e31b1e2a3b4618ce406d5ab
RigodLee <852195914@qq.com>
2020-10-03 12:16:24 +0800

    Site updated: 2020-10-03 12:16:03

--- js/shadow.js
+++ js/shadow.js
@@ -1,1 +1,10 @@
-$("#framework-info").attr("style","display:none;");//隐藏div
+// $("#framework-info").attr("style","display:none;");//隐藏div
+
+$(document).ready(function(e){
+        $('.framework-info').html('shadow - DEBUG THE WORLD');
+    })
+
+// 设置页脚博主会动的心
+$(document).ready(function(e){
+    $('.copyright').html('©2019 - 2020 <i style="color:#FF6A6A;animation: announ_animation 0.8s linear infinite;" class="fa fa-heartbeat"></i> shadow');
+})
