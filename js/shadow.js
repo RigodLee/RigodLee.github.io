8d9dd164614142aee4dd46c831b1bc2fc0e30735
RigodLee <852195914@qq.com>
2020-10-04 14:52:12 +0800

    Site updated: 2020-10-04 14:52:00

--- js/shadow.js
+++ js/shadow.js
@@ -71,8 +71,10 @@ function show_date_time(){
     }
     show_date_time();
 
-// up酱
-/*添加图片top*/
-var top_up = "<img id='upj' class='upj' style='max-width: 1000%; transform: translate(-70px,-70px);' src='https://cdn.jsdelivr.net/gh/rigodlee/BLOGCDN/img/upDoraemon.gif' title='回到顶部' >";
-/*添加到返回顶部按钮下*/
-document.getElementById("go-up").innerHTML += top_up;
+
+
+$(document).ready(function(e){
+    var top_up = "<img id='upj' class='upj' style='max-width: 1000%; transform: translate(-70px,-70px);' src='https://cdn.jsdelivr.net/gh/rigodlee/BLOGCDN/img/upDoraemon.gif' title='回到顶部' >";
+
+    $('#go-up').innerHTML += top_up;
+})
