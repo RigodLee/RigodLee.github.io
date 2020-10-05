1ba86d4fdf7f5c8437b0ac6dcb93a94b2c31f0a7
RigodLee <852195914@qq.com>
2020-10-05 12:18:44 +0800

    Site updated: 2020-10-05 12:18:30

--- js/shadow.js
+++ js/shadow.js
@@ -85,4 +85,7 @@ function show_date_time(){
 
 // add_footerImage();
 
-$("#footer").append('<div id="footer-img"><img src="https://cdn.jsdelivr.net/gh/rigodlee/BLOGCDN/img/e92eef6d3d32097f37c9da214a6203e4.png"></div>');
+
+$(document).ready(function(e){
+    $("#footer").append('<div id="footer-img"><img src="https://cdn.jsdelivr.net/gh/rigodlee/BLOGCDN/img/e92eef6d3d32097f37c9da214a6203e4.png"></div>');
+})
